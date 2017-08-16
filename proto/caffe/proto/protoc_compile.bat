cbeaf46cf6708c235ae69e5a344f3d70a4029990
bgm <gmbak@postech.ac.kr>
2017-08-16 14:55:04 +0900

    add attribute 'colored' to message ImgBBoxAnnoParam (modify protoc_compile.bat)

--- proto/caffe/proto/protoc_compile.bat
+++ proto/caffe/proto/protoc_compile.bat
@@ -1,6 +1,8 @@
-set IMPORT_DIR=D:/library/caffe_env/caffe/include
-set SRC_DIR=D:/workspace/TLR/caffe/proto
-set DST_DIR=D:/workspace/TLR/caffe/proto
+rem set IMPORT_DIR=D:/library/caffe_env/caffe/include
+set PROTOC=D:/dev_tools/protobuf/protoc-3.1.0-win32/bin/protoc
+set SRC_DIR=D:\workspace\TLR\caffe\proto\caffe\proto
+set DST_DIR=D:\workspace\TLR\caffe\proto\caffe\proto
 set PROTO_FILE=caffe.proto
 
-D:/dev_tools/protobuf/protoc-3.1.0-win32/bin/protoc -I=%IMPORT_DIR% -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%/%PROTO_FILE%
+rem D:/dev_tools/protobuf/protoc-3.1.0-win32/bin/protoc -I=%IMPORT_DIR% -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%/%PROTO_FILE%
+%PROTOC% -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%/%PROTO_FILE%
