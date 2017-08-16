3f0cdde8a8937ca6a4da762f0542b8de749065ee
bgm <gmbak@postech.ac.kr>
2017-08-16 19:39:02 +0900

    create class BBoxAnnoMapLayer

---
+++ inc/bbox_anno_map_layer.hpp
@@ -0,0 +1,17 @@
+#ifndef TRL_BBOX_ANNO_MAP_LAYER_HPP_
+#define TRL_BBOX_ANNO_MAP_LAYER_HPP_
+
+#include "caffe/layer.hpp"
+
+namespace caffe
+{
+
+template <typename Dtype>
+class BBoxAnnoMapLayer : public Layer<Dtype>
+{
+
+};
+
+} // namespace caffe
+
+#endif // !TRL_BBOX_ANNO_MAP_LAYER_HPP_
---
+++ src/bbox_anno_map_layer.cpp
@@ -0,0 +1,6 @@
+#include "bbox_anno_map_layer.hpp"
+
+namespace caffe
+{
+
+} // namespace caffe
