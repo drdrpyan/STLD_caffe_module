6d5840835815a7137580396d159afa30710dda1c
bgm <gmbak@postech.ac.kr>
2017-09-13 22:13:52 +0900

    create class BBoxToGridAndSizeLayer

---
+++ inc/bbox_to_grid_and_size_layer.hpp
@@ -0,0 +1,109 @@
+#ifndef TLR_BBOX_TO_GRID_AND_SIZE_LAYER_HPP_
+#define TLR_BBOX_TO_GRID_AND_SIZE_LAYER_HPP_
+
+#include "caffe/layer.hpp"
+
+namespace caffe
+{
+
+template <typename Dtype>
+class BBoxToGridAndSizeLayer : public Layer<Dtype>
+{
+ public:
+  explicit BBoxToGridAndSizeLayer(const LayerParameter& param);
+  virtual void LayerSetUp(
+      const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) override;
+  virtual void Reshape(
+      const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) override;
+  virtual const char* type() const;
+  virtual int ExactNumBottomBlobs() const override;
+  virtual int ExactNumTopBlobs() const override;
+
+ protected:
+  virtual void Forward_cpu(
+      const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) override;
+  virtual void Forward_gpu(
+      const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) override;
+
+  /**
+  This layer handles input data. It does not backprogate.
+  */
+  virtual void Backward_cpu(
+      const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, 
+      const vector<Blob<Dtype>*>& bottom) override;
+  /**
+  This layer handles input data. It does not backprogate.
+  */
+  virtual void Backward_gpu(
+      const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, 
+      const vector<Blob<Dtype>*>& bottom) override;
+
+ private:
+  int GetGridIdx(Dtype x_min, Dtype y_min, Dtype width, Dtype height) const;
+  void GetBBoxCenter(Dtype x_min, Dtype y_min, Dtype width, Dtype height,
+                     Dtype* center_x, Dtype* center_y) const;
+  Dtype GetSize(Dtype size) const;
+
+  BBoxParameter::BBoxType bbox_type_;
+  std::vector<Dtype> x_grid_;
+  std::vector<Dtype> y_grid_;
+  std::vector<Dtype> size_grid_;
+};
+
+// inline functions
+template <typename Dtype>
+inline const char* BBoxToGridAndSizeLayer<Dtype>::type() const {
+  return "BBoxToGridAndSize";
+}
+
+template <typename Dtype>
+inline int BBoxToGridAndSizeLayer<Dtype>::ExactNumBottomBlobs() const {
+  return 1;
+}
+
+template <typename Dtype>
+inline int BBoxToGridAndSizeLayer<Dtype>::ExactNumTopBlobs() const {
+  return 2;
+}
+
+template <typename Dtype>
+inline void BBoxToGridAndSizeLayer<Dtype>::Forward_gpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  Forward_cpu(bottom, top);
+}
+
+template <typename Dtype>
+inline void BBoxToGridAndSizeLayer<Dtype>::Backward_cpu(
+  const vector<Blob<Dtype>*>& top,
+  const vector<bool>& propagate_down,
+  const vector<Blob<Dtype>*>& bottom) {
+
+}
+
+template <typename Dtype>
+inline void BBoxToGridAndSizeLayer<Dtype>::Backward_gpu(
+  const vector<Blob<Dtype>*>& top,
+  const vector<bool>& propagate_down,
+  const vector<Blob<Dtype>*>& bottom) {
+
+}
+
+template <typename Dtype>
+inline void BBoxToGridAndSizeLayer<Dtype>::GetBBoxCenter(
+    Dtype x_min, Dtype y_min, Dtype width, Dtype height,
+    Dtype* center_x, Dtype* center_y) const {
+  CHECK(center_x);
+  CHECK(center_y);
+
+  center_x = x_min + (width / 2.0);
+  center_y = y_min + (height / 2.0);
+}
+
+} // namespace caffe
+#endif // !TLR_BBOX_TO_GRID_AND_SIZE_LAYER_HPP_
---
+++ src/bbox_to_grid_and_size_layer.cpp
@@ -0,0 +1,110 @@
+#include "bbox_to_grid_and_size_layer.hpp"
+
+namespace caffe
+{
+
+template <typename Dtype>
+BBoxToGridAndSizeLayer<Dtype>::BBoxToGridAndSizeLayer(
+  const LayerParameter& param)
+  : Layer<Dtype>(param) {
+
+}
+
+template <typename Dtype>
+void BBoxToGridAndSizeLayer<Dtype>::LayerSetUp(
+  const vector<Blob<Dtype>*>& bottom,
+  const vector<Blob<Dtype>*>& top) {
+  bbox_type = layer_param().bbox_param().bbox_type;
+  CHECK(bbox_type == BBoxParameter::VERTICAL ||
+        bbox_type == BBoxParameter::HORIZONTAL) << "Illegal bbox type.";
+
+  const BBoxToGridAndSizeParameter& param = layer_param().bbox_to_gird_and_size_param();
+  CHECK_EQ(param.x_grid().size(), param.y_grid().size());
+  x_grid_.clear();
+  y_grid_.clear();
+  for (int i = 0; i < param.x_grid().size(); i++) {
+    x_grid_.push_back(param.x_grid().Get(i));
+    y_grid_.push_back(param.y_grid().Get(i));
+  }
+  std::sort(x_grid_.begin(), x_grid_.end());
+  std::sort(y_grid_.begin(), y_grid_.end());
+
+  size_grid_.clear();
+  for (int i = 0; i < param.size_grid().size(); i++)
+    size_grid_.push_back(param.size_grid().Get(i));
+  std::sort(size_grid_.begin(), size_grid_.end());
+}
+
+template <typename Dtype>
+void BBoxToGridAndSizeLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+                                            const vector<Blob<Dtype>*>& top) {
+  const Blob<Dtype>& bbox_input = *(bottom[0]);
+  Blob<Dtype>& grid_out = *(top[0]);
+  Blob<Dtype>& size_out = *(top[1]);
+
+  std::vector<int> grid_out_shape = bbox_input.shape();
+  grid_out_shape[1] = (x_grid_.size() + 1) * (y_grid_.size() + 1);
+  grid_out.Reshape(grid_out_shape);
+
+  std::vector<int> size_out_shape = bbox_input.shape();
+  size_out_shape[1] = size_grid_.size() + 1;
+  size_out.Reshape(size_out_shape);
+}
+
+template <typename Dtype>
+void BBoxToGridAndSizeLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  const Blob<Dtype>& bbox_input = *(bottom[0]);
+  Blob<Dtype>& grid_out = *(top[0]);
+  Blob<Dtype>& size_out = *(top[1]);
+  
+  for (int n = 0; n < bbox_input.num(); n++) {
+    const Dtype* x_iter = bbox_input.cpu_data() + bbox_input.offset(n, 0);
+    const Dtype* y_iter = bbox_input.cpu_data() + bbox_input.offset(n, 1);
+    const Dtype* w_iter = bbox_input.cpu_data() + bbox_input.offset(n, 2);
+    const Dtype* h_iter = bbox_input.cpu_data() + bbox_input.offset(n, 3);
+    Dtype* grid_out_iter = grid_out.mutable_cpu_data() + grid_out.offset(n);
+    Dtype* size_out_iter = size_out.mutable_cpu_data() + size_out.offset(n);
+
+    for (int i = bbox_input.height() * bbox_input.width(); i--; ) {
+      *grid_out_iter = GetGridIdx(*x_iter, *y_iter, *w_iter, *h_iter);
+      *size_out_iter = GetSize((bbox_type_ == BBoxParameter::HORIZONTAL)
+                               ? *h_iter : *w_iter);
+
+      ++x_iter;
+      ++y_iter;
+      ++w_iter;
+      ++h_iter;
+      ++grid_out_iter;
+      ++size_out_iter;
+    }
+  }
+}
+
+template <typename Dtype>
+int BBoxToGridAndSizeLayer<Dtype>::GetGridIdx(Dtype x_min, Dtype y_min,
+                                              Dtype width, Dtype height) const {
+  Dtype center_x, center_y;
+  GetBBoxCenter(x_min, y_min, width, height, *center_x, *center_y);
+  
+  int x_idx, y_idx;
+  for (x_idx = 0; x_idx < x_grid_.size() && center_x >= x_grid_[x_idx]; x_idx++);
+  for (y_idx = 0; y_idx < y_grid_.size() && center_y >= y_grid_[y_idx]; y_idx++);
+
+  int grid_idx = y_idx * (x_grid_.size() + 1) + x_idx;
+}
+
+template <typename Dtype>
+Dtype BBoxToGridAndSizeLayer<Dtype>::GetSize(Dtype size) const {
+  if (size_grid_.size() == 0)
+    return size;
+  else {
+    int size_idx;
+    for (size_idx = 0; 
+         size_idx < size_grid_.size() && size >= size_grid_[size_idx];
+         size_idx++);
+    return static_cast<Dtype>(size_idx);
+  }
+}
+
+} // namespace caffe
